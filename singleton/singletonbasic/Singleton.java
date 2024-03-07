41be3bf513299e4fdd9c3a53258f88ef7537f787
root <root@abin.gov.br>
2024-03-07 13:25:17 -0300

    Singleton

---
+++ singleton/singletonbasic/Singleton.java
@@ -0,0 +1,24 @@
+package singleton.singletonbasic;
+
+public class Singleton {
+    
+    private String data;
+    private static Singleton instance;
+
+    private Singleton (String data){
+        this.data = data;
+    }
+
+    public static Singleton getInstance(String data) {
+        if (instance == null){
+            instance = new Singleton(data);
+        }
+        
+        return instance;
+    }
+
+    public String getData() {
+        return this.data;
+    }
+
+}
---
+++ singleton/singletonbasic/Teste.java
@@ -0,0 +1,11 @@
+package singleton.singletonbasic;
+
+public class Teste {
+    
+    public static void main (String[] args) {
+       Singleton s1 = Singleton.getInstance("dado original");    
+       Singleton s2 = Singleton.getInstance("Novo dado");    
+       System.out.println(s2 == s1 ? "Criada apenas uma instância" : "Criadas mais de uma instância");
+
+    }
+}
